6d74a585dffa4a01c655ae07d6c6211d961f7670
Vlad290896 <vlad_karp@outlook.com>
2023-04-06 17:38:22 +0300

    New Changes

--- Main_Files/TEMA_2/SQL_Files_TEMA2/Analiza_Date_1.sql
+++ Main_Files/TEMA_2/SQL_Files_TEMA2/Analiza_Date_1.sql
@@ -1,69 +1,73 @@
-SELECT * from patients_gen_health_state;
-SELECT * from mts_to_view;
-SELECT * from countries_view;
-select * from patients_view;
+SELECT * FROM patients_gen_health_state;
+SELECT * FROM mts_to_view;
+SELECT * FROM countries_view;
+SELECT * FROM patients_view;
 
 -----------------------------------------------------------
+
 DROP VIEW OLAP_FACT_FINAL_HEALTH_STATE;
 CREATE OR REPLACE VIEW OLAP_FACT_FINAL_HEALTH_STATE AS
 SELECT P.Subject, PV.Symptoms
     , SUM(P.Health_State) as Final_Health_State
 FROM patients_gen_health_state P 
     INNER JOIN patients_view PV
-        ON P.Subject =  PV.idpacient
+        ON P.subject =  PV.subject
 GROUP BY P.Subject, PV.Symptoms;
 
-SELECT * FROM OLAP_FINAL_HEALTH_STATE;
+SELECT * FROM OLAP_FACT_FINAL_HEALTH_STATE;
 
 ------------------------------------------------------------
 
-DROP VIEW OLAP_DIM_SUBJ_SITE_COUNTRY;
-CREATE OR REPLACE VIEW OLAP_DIM_SUBJ_SITE_COUNTRY AS
-SELECT
-    PV.Subject as subject,
+DROP VIEW OLAP_DIM_SUBJ_SIT_COUNTRY;
+CREATE OR REPLACE VIEW OLAP_DIM_SUBJ_SIT_COUNTRY AS
+SELECT 
+    PV.subject as subject,
     CV.countryName as countryName
 FROM patients_view PV 
-    INNER JOIN countries_view CV ON CV.subject = PV.idpacient;
-    
-SELECT * FROM OLAP_DIM_SUBJ_SITE_COUNTRY;
+    INNER JOIN countries_view CV ON PV.subject = CV.subject;
+SELECT * FROM OLAP_DIM_SUBJ_SIT_COUNTRY;
+
 
 ------------------------------------------------------------
 
 DROP VIEW OLAP_DIM_SUBJ_AGE_VISITS;
 CREATE OR REPLACE VIEW OLAP_DIM_SUBJ_AGE_VISITS AS
 SELECT 
-	PV.Subject as subject,
+	PV.subject as subject,
 	PV.Age as age,
 	P.VIS as vis
 FROM patients_view PV 
-	INNER JOIN patients_gen_health_state P ON P.Subject = PV.idpacient;
+	INNER JOIN patients_gen_health_state P ON PV.idpacient = P.subject;
     
 -------------------------------------------------------------
 
-DROP VIEW OLAP_DIM_SUBJ_REGION_OPINION;
-CREATE OR REPLACE VIEW OLAP_DIM_SUBJ_REGION_OPINION AS
+DROP VIEW OLAP_DIM_SUBJ_COUNTRY_OPINION;
+CREATE OR REPLACE VIEW OLAP_DIM_SUBJ_COUNTRY_OPINION AS
 SELECT 
-	PV.idpacient as idpacient,
+	MTS.subject as subject,
+    CV.countryName as country_name,
 	MTS.MTSRES1 as gen_opinion
 FROM
-	patients_view PV
-	INNER JOIN countries_view CV ON CV.subject = PV.idpacient
-	INNER JOIN mts_to_view MTS ON MTS.subject = PV.idpacient
-WHERE MTS.MTSRES1 NOT LIKE '%agree%';
+	countries_view CV
+	INNER JOIN mts_to_view MTS ON CV.subject = MTS.subject
+WHERE MTS.MTSRES1 LIKE 'AGREE%';
 
-select * from OLAP_DIM_SUBJ_REGION_OPINION;
+select * from OLAP_DIM_SUBJ_COUNTRY_OPINION;
+SELECT COUNT(MTSRES1) AS OPINIONS FROM mts_to_view WHERE MTSRES1 LIKE 'AGREE%';
+
+
 
 -------------------------------------------------------------
 
-DROP VIEW OLAP_VIEW_FHEALTH_STATE_SITE_COUNTRIES;
-CREATE OR REPLACE VIEW OLAP_VIEW_FHEALTH_STATE_SITE_COUNTRIES AS
+DROP VIEW OLAP_VIEW_FHEALTH_STATE_COUNTRIES;
+CREATE OR REPLACE VIEW OLAP_VIEW_FHEALTH_STATE_COUNTRIES AS
 SELECT 
 CASE
     WHEN GROUPING(D1.subject) = 1 THEN '{Total General}'
     ELSE cast(D1.subject as varchar2(20)) END AS subject,
   CASE 
     WHEN GROUPING(D1.subject) = 1 THEN ' '
-    WHEN GROUPING(D1.countryName) = 1 THEN 'Subtotal countries ' || D1.subject
+    WHEN GROUPING(D1.countryName) = 1 THEN 'Subtotal ' || D1.subject
     ELSE D1.countryName END AS countryName,
   CASE 
     WHEN GROUPING(D1.subject) = 1 THEN ' '
@@ -71,15 +75,15 @@ CASE
     WHEN GROUPING(D1.subject) = 1 THEN 'subtotal country ' || D1.countryName
     ELSE to_char(D1.subject) END AS Subject_Identifier,    
   SUM(NVL(f.Final_Health_State, 0)) as Final_Health_State   
-FROM OLAP_DIM_SUBJ_SITE_COUNTRY D1
+FROM OLAP_DIM_SUBJ_SIT_COUNTRY D1
     INNER JOIN OLAP_FACT_FINAL_HEALTH_STATE F ON D1.subject = F.subject
 GROUP BY ROLLUP (d1.subject, d1.countryName)
 ORDER BY d1.subject, d1.countryName;
 
-select * from OLAP_DIM_SUBJ_SITE_COUNTRY;
+select * from OLAP_FACT_FINAL_HEALTH_STATE;
+select * from OLAP_DIM_SUBJ_SIT_COUNTRY;
 
 ----------------------------------------------------------------------------
-
 DROP VIEW OLAP_VIEW_FHEALTH_STATE_AGE_GENDER;
 CREATE OR REPLACE VIEW OLAP_VIEW_FHEALTH_STATE_AGE_GENDER AS
 SELECT 
@@ -104,6 +108,7 @@ ORDER BY d2.VIS, d2.age, d2.Subject;
 select *  FROM OLAP_VIEW_FHEALTH_STATE_AGE_GENDER; 
 
 ---------------------------------------------------------------------------
+
 DROP VIEW OLAP_DIM_SUBJ_AGE_GENDER;
 CREATE OR REPLACE VIEW OLAP_DIM_SUBJ_AGE_GENDER AS
 SELECT 
@@ -118,8 +123,8 @@ FROM patients_view PV
 
 ---------------------------------------------------------------------------
 
-DROP VIEW OLAP_VIEW_FHEALTH_STATE_REG_AGE_GENDER;
-CREATE OR REPLACE VIEW OLAP_VIEW_FHEALTH_STATE_REG_AGE_GENDER AS
+DROP VIEW OLAP_VIEW_FHEALTH_STATE_CTY_AGE_GENDER;
+CREATE OR REPLACE VIEW OLAP_VIEW_FHEALTH_STATE_CTY_AGE_GENDER AS
 SELECT 
 CASE
     WHEN GROUPING(D3.countryName) = 1 THEN '{Total General}'
@@ -143,26 +148,26 @@ SELECT * FROM OLAP_FACT_FINAL_HEALTH_STATE;
 SELECT * FROM OLAP_DIM_SUBJ_AGE_GENDER;
 
 ---------------------------------------------------------------------------
-DROP VIEW OLAP_VIEW_FHEALTH_STATE_REG_OPINION;
-CREATE OR REPLACE VIEW OLAP_VIEW_FHEALTH_STATE_REG_OPINION AS
+DROP VIEW OLAP_VIEW_FHEALTH_STATE_CTY_OPINION;
+CREATE OR REPLACE VIEW OLAP_VIEW_FHEALTH_STATE_CTY_OPINION AS
 SELECT
 CASE
     WHEN GROUPING(C.countryName) = 1 THEN '{Total General}'
     ELSE C.countryName END AS countryName,
 CASE
     WHEN GROUPING(C.countryName) = 1 THEN ' '
-    WHEN GROUPING(D4.idpacient) = 1 THEN 'subtotal regiune' || to_char(D4.idpacient)
-    ELSE to_char(D4.idpacient) END AS Subject_Identifier,
+    WHEN GROUPING(D4.subject) = 1 THEN 'subtotal regiune' || to_char(D4.subject)
+    ELSE to_char(D4.subject) END AS Subject_Identifier,
     COUNT(*) AS Total_Patients
-FROM OLAP_DIM_SUBJ_REGION_OPINION D4
-    INNER JOIN OLAP_DIM_SUBJ_SITE_COUNTRY C ON D4.idpacient = C.subject
-    INNER JOIN olap_dim_subj_region_opinion O ON C.subject = O.idpacient
+FROM OLAP_DIM_SUBJ_COUNTRY_OPINION D4
+    INNER JOIN OLAP_DIM_SUBJ_SIT_COUNTRY C ON D4.subject = C.subject
+    INNER JOIN olap_dim_subj_country_opinion O ON C.subject = O.subject
 GROUP BY
-    ROLLUP(C.countryName, D4.idpacient)
+    ROLLUP(C.countryName, D4.subject)
 ORDER BY
-    C.countryName, D4.idpacient;
+    C.countryName, D4.subject;
 
-select * from olap_dim_subj_region_opinion;
+select * from olap_dim_subj_country_opinion;
 
 ---------------------------------------------------------------------------
 
@@ -218,7 +223,7 @@ ORDER BY 1;
 
 SELECT P.subject, SUM(P.health_state) AS health_state
 FROM patients_view PV 
-INNER JOIN patients_gen_health_state P ON PV.idpacient = P.subject
+INNER JOIN patients_gen_health_state P ON PV.subject = P.subject
 GROUP BY P.subject
 ORDER BY SUM(P.health_state) DESC;
 
@@ -230,11 +235,11 @@ SELECT
     CV.countryName,
     SUM(P.health_state) AS final_health_state
 FROM patients_gen_health_state P
-    INNER JOIN patients_view PV ON P.subject = PV.idpacient
-    INNER JOIN countries_view CV ON PV.idpacient = CV.subject
+    INNER JOIN patients_view PV ON P.subject = PV.subject
+    INNER JOIN countries_view CV ON PV.subject = CV.subject
 GROUP BY CUBE(P.subject, P.sit, CV.countryName)
 ORDER BY 1,2
-FETCH NEXT 31 ROWS ONLY;
+FETCH NEXT 97 ROWS ONLY;
 
 
 
